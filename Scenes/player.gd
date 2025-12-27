e77b98dcba6b5153f0e424e7fac515cb259af5cc
Tim van Heumen <tim@vhweb.nl>
2025-12-27 10:52:36 +0100

    Added player and main scene

---
+++ Scenes/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	velocity.x = 200
+	
+	velocity += get_gravity() * delta
+
+	velocity += Vector2(0, -400) * delta
+	
+	if Input.is_action_just_pressed("flap"):
+		velocity.y = -200
+	
+
+	if velocity.y > 1000:
+		velocity.y = 1000
+
+	print(velocity)
+	move_and_slide()
